a481c577d1b497eb54a67442a8c6cdaae425e55b
yuanyuanjun <yuanyuanjun@ascs.tech>
2018-03-06 23:53:26 +0800

    refactor:RTC.js代码重构

--- src/pages/chat-media/RTC.js
+++ src/pages/chat-media/RTC.js
@@ -1,132 +1,160 @@
-var PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+let PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+let iceServer = {
+    "iceServers": [
+        {
+            "urls": "stun:106.15.225.7:3478"
+        },
+        {
+            "urls": "turn:106.15.225.7:3478",
+            "username": "user",
+            "credential": "password",
+        },
+    ]
+};//p2p连接服务
+let mediaModel; //数据缓存区
+
 export default {
-    install(localVideo, remoteVideo, isCaller) {
-        chatTool.isCaller = isCaller;
-        chatTool.remoteVideoPlayer = remoteVideo;
-        chatTool.localVideoPlayer = localVideo;
-        return new Promise(function (res, rej) {
-            chatTool.installRes = res;
-            chatTool.pc = new PeerConnection(chatTool.iceServer);
-            chatTool.pc.ontrack = chatTool.showRemoteVideo;
-            chatTool.pc.onicecandidate = chatTool.saveLocalCD;
-            chatTool.getMedia();
-        })
+    install(localVideo, remoteVideo) {
+        return new Promise(function (rs,rj) {
+            mediaModel.remoteVideoPlayer = remoteVideo;
+            mediaModel.localVideoPlayer = localVideo;
+            mediaModel = {
+                res:null,
+                iceGatheringState:null,//本地cd收集状态
+                remoteVideoPlayer: null,//播放远程视频的vedio标签
+                localVideoPlayer: null,//播放本地视频的vedio标签
+                pc: null,//PeerConnection实例
+                localCd: [],//本地candidate
+                remoteCd: [],//远程candidate
+                localSdp: null,//本地description
+                remoteSdp: null,//远程description
+            };
+            mediaModel.pc = new PeerConnection(iceServer);
+            mediaModel.pc.ontrack = showRemoteVideo;
+            mediaModel.pc.onicecandidate = saveLocalCD;
+            rs();
+        });
+    },
+    caller: {
+        setLocal(options = {audio: false, video: true}) {
+            return new Promise(function (res, rej) {
+                getMedia(options)
+                    .then(linkLocalVideo)
+                    .then(createOffer)
+                    .then(localCompete)
+                    .catch();
+            });
+        },
+        setRemote(remoteSdp, remoteCd) {
+            mediaModel.remoteSdp = remoteSdp;
+            mediaModel.remoteCd = remoteCd;
+            return new Promise(function (rs, rj) {
+                setRemoteSdp()
+                    .then(addRemoteCd)
+                    .then(rs)
+                    .catch(rj)
+            })
+        },
+    },
+    answerer: {
+        setRemote(options = {audio: false, video: true}, remoteSdp, remoteCd) {
+            mediaModel.remoteSdp = remoteSdp;
+            mediaModel.remoteCd = remoteCd;
+            return new Promise(function (res, rej) {
+                getMedia(options)
+                    .then(linkLocalVideo)
+                    .then(setRemoteSdp)
+                    .then(addRemoteCd)
+                    .then(createAnswer)
+                    .then(localCompete)
+                    .catch();
+            });
+        },
     },
 };
-var chatTool = {
-    installRes:null,
-    isCaller: false,//是否主动呼叫者
-    remoteVideoPlayer: null,//播放远程视频的vedio标签
-    localVideoPlayer: null,//播放本地视频的vedio标签
-    iceServer: {
-        "iceServers": [
-            {
-                "urls": "stun:106.15.225.7:3478"
-            },
-            {
-                "urls": "turn:106.15.225.7:3478",
-                "username": "user",
-                "credential": "password",
-            },
-        ]
-    },//p2p连接服务
-    pc: null,//PeerConnection实例
-    localCd: [],//本地candidate
-    remoteCd: [],//远程candidate
-    localSdp: null,//本地description
-    remoteSdp: null,//远程description
-    //获取摄像头权限
-    getMedia() {
-        chatTool.isCaller = true;
-        navigator
-            .mediaDevices.getUserMedia({audio: false, video: true})
-            .then(chatTool.onLocalMediaStart)
-            .catch(error => {
-                console.error("mediaStart,getUserMedia#" + error)
-            });
-    },
-    //获取到了摄像头权限，
-    onLocalMediaStart(mediaStream) {
-        chatTool.pc.addStream(mediaStream);
-        chatTool.localVideoPlayer.srcObject = mediaStream;
-        chatTool.setLocalSdpComfirm();
-    },
-    //生成本地sdp，判断是offer还是answer
-    setLocalSdpComfirm() {
-        var pc = chatTool.pc;
-        var that = chatTool;
-        if (!chatTool.isCaller) {
-            pc.createAnswer(chatTool.setLocalSdp, error => {
-                console.error("mediaStart,createOffer#" + error)
-            });
-        } else {
-            pc.createOffer(chatTool.setLocalSdp, error => {
-                console.error("mediaStart,createOffer#" + error)
-            });
+//播放远程视频
+function showRemoteVideo(evt) {
+    mediaModel.remoteVideoPlayer.srcObject = evt.streams[0];
+};
+
+//保存pc.oncandidate返回的本地cd
+function saveLocalCD(evt) {
+    let state = mediaModel.iceGatheringState = evt.target.iceGatheringState;
+    (state === "complete")
+        ? localCompete()
+        : mediaModel.localCd.push(evt.candidate);
+};
+
+//获取摄像头权限
+function getMedia(options) {
+    return navigator.mediaDevices.getUserMedia(options); //promise
+};
+
+//播放、链接本地视频
+function linkLocalVideo(mediaStream) {
+    return new Promise(function (rs, rj) {
+        mediaModel.pc.addStream(mediaStream);
+        mediaModel.localVideoPlayer.srcObject = mediaStream;
+        rs();
+    })
+};
+
+//将后台发过来的cd设置进pc
+function addRemoteCd() {
+    mediaModel.remoteCd.forEach((item, index) => {
+        mediaModel.pc.addIceCandidate(new RTCIceCandidate(item));
+        if (index === this.length - 1) {
+            return mediaModel.pc.addIceCandidate(new RTCIceCandidate(item)) //promise
         }
-    },
-    //生成本地sdp
-    setLocalSdp (sdp) {
-        chatTool.pc.setLocalDescription(sdp, function () {
-            let localSdp = chatTool.pc.localDescription;
-            chatTool.saveLocalSdp(localSdp);
+    })
+};
+
+//将后台发送过来了的sdp设置为pc的remoteSdp
+function setRemoteSdp() {
+    new Promise(function (rs, rj) {
+        mediaModel.pc.setRemoteDescription(mediaModel.remoteSdp, rs, rj);
+    })
+};
+
+//caller setLocal
+function createOffer() {
+    return new Promise(function (rs, rj) {
+        let pc = mediaModel.pc;
+        pc.createOffer(
+            function (sdp) {
+                pc.setLocalDescription(sdp, ()=>{
+                        mediaModel.localSdp = pc.localDescription;
+                        rs();
+                    }, error => {console.error("mediaStart,setLocalDescription#" + error)});
+            }, error => {console.error("mediaStart,createOffer#" + error)}
+        );
+    })
+};
+
+//answerer setLocal
+function createAnswer() {
+    return new Promise(function (rs, rj) {
+        let pc = mediaModel.pc;
+        pc.createAnswer(function (sdp) {
+            pc.setLocalDescription(sdp, function () {
+                let localSdp = pc.localDescription;
+                mediaModel.localSdp = localSdp;
+                rs();
+            }, error => {
+                console.error("mediaStart,setLocalDescription#" + error)
+            });
         }, error => {
-            console.error("mediaStart,setLocalDescription#" + error)
+            console.error("mediaStart,createOffer#" + error)
         });
-    },
-    //保存生成的本地sdp
-    saveLocalSdp(sdp) {
-        chatTool.localSdp = sdp;
-        if(chatTool.iceLocalState === "complete"){
-            let param = {sdp:chatTool.localSdp,cd:chatTool.localCd};
-            chatTool.installRes(param);
-        }
-    },
-    //保存pc.oncandidate返回的cd
-    saveLocalCD(evt) {
-        chatTool.iceLocalState = evt.target.iceGatheringState;
-        (evt.target.iceGatheringState === "complete")
-            ? chatTool.completeLocalCd(chatTool.localCd)
-            : chatTool.localCd.push(evt.candidate);
-    },
-    //展示LocalCd
-    completeLocalCd(data) {
-        if(chatTool.localSdp){
-            let param = {sdp:chatTool.localSdp,cd:chatTool.localCd};
-            chatTool.installRes(param);
-        }
-    },
-    //接收从后台发过来的cd
-    saveRemoteCd(evt) {
-        if (evt.isCaller !== chatTool.isCaller) {
-            chatTool.remoteCd = evt.cd;
-        }
-    },
-    //将后台发过来的cd设置进pc
-    addRemoteCd() {
-        chatTool.remoteCd.forEach(item => {
-            chatTool.pc.addIceCandidate(new RTCIceCandidate(item))
-                .then(function () {
-                    console.log("addIceCandidate");
-                })
-                .catch(function (err) {
-                    console.log("addIceCandidate failed", err)
-                })
-        })
-    },
-    //保存后台发送过来的sdp
-    saveRemoteSdp(sdp) {
-        chatTool.remoteSdp = sdp;
-    },
-    //将后台发送过来了的sdp设置为pc的remoteSdp
-    setRemoteSdp() {
-        chatTool.pc.setRemoteDescription(chatTool.remoteSdp, function () {
-            chatTool.setLocalSdp();
-        }, function (err) {
-        });
-    },
-    //播放远程视频
-    showRemoteVideo(evt) {
-        chatTool.remoteVideoPlayer.srcObject = evt.streams[0];
-    },
+    })
 };
+
+//本地cd,sdp准备完毕
+function localCompete() {
+    if(!mediaModel.iceGatheringState || !mediaModel.localSdp) return;
+    let params = {
+        sdp:mediaModel.localSdp,
+        cd:mediaModel.localCd,
+    };
+    mediaModel.res(params);
+}
--- src/pages/chat-media/media-route.vue
+++ src/pages/chat-media/media-route.vue
@@ -7,15 +7,17 @@
     export default {
         data() {
             return {
+                isCaller: false,
                 chatSocket: null,
-                user:null,
-                roomId:"",
+                user: null,
+                roomId: "",
             }
         },
         methods: {
             chatMediaHandle(user, chatType, roomId) {
                 switch (chatType.id) {
                     case "video":
+                        this.isCaller = true;
                         return this.videoChatHandle(user, roomId);
                     case "audio":
                         return this.audioChatHandle(user, roomId);
@@ -23,37 +25,47 @@
                         return;
                 }
             },
-            videoChatHandle(user, roomId) {
+            videoChatHandle(user, roomId, sdp, cd) {
                 this.roomId = roomId;
-                this.targetUser= user;
+                this.targetUser = user;
                 this.$router.push({name: "视频通话"});
                 let remoteVideo = document.querySelector("#remoteVideo");
                 let localVideo = document.querySelector("#localVideo");
-                let isCaller = true;
-                RtcTool.install(localVideo, remoteVideo, isCaller)
+                RtcTool.install(localVideo, remoteVideo, this.isCaller, sdp, cd)
                     .then(this.sendLocalRtc);
             },
             sendLocalRtc(params) {
                 let mes = {
-                    type:"chatVideo",
-                    roomId:this.roomId,
-                    targetUser:{
-                        nickname:this.targetUser.nickname,
-                        userId:this.targetUser.userId,
+                    type: "chatVideo",
+                    roomId: this.roomId,
+                    targetUser: {
+                        nickname: this.targetUser.nickname,
+                        userId: this.targetUser.userId,
                     },
-                    data:params,
+                    data: params,
                 };
-                window.SYRESOURCE.chatSocket.emit("clientMes",mes);
+                window.SYRESOURCE.chatSocket.emit("clientMes", mes);
+            },
+            getRemoteRtc(mes) {
+                if (mes.type !== "chatVideo") return;
+                this.isCaller
+                    ? this.videoChatHandle(mes.userInfo, mes.roomId,mes.data.sdp,mes.data.cd)
+                    : RtcTool.setRemote(mes.data.sdp,mes.data.cd);
             },
             audioChatHandle(user, roomId) {
                 this.$router.push({name: "语音通话"});
             },
+
         },
         mounted() {
             this._$eventBus.$on("chatMedia", this.chatMediaHandle);
+            this._$eventBus.$on("chatSocketConnected",()=>{
+                window.SYRESOURCE.chatSocket.on("serverMes", this.getRemoteRtc);
+            });
         },
         destroyed() {
             this._$eventBus.$off("chatMedia");
+            this._$eventBus.$off("chatSocketConnected");
         }
     }
 </script>
